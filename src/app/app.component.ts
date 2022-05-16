9ae7e0e0cc2f3d1f108941e5908db0add6fe6236
thao02122002 <vant16039@gmail.com>
2022-05-16 11:22:31 +0700

    form thêm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent {
 
    teachers = [
      {
-       id: '123',
+       id: 123,
        name: "loan",
        age: 35,
        gender: 0,
@@ -33,7 +33,7 @@ export class AppComponent {
        status: 1
      },
      {
-      id: '124',
+      id: 124,
       name: "nhật",
       age: 42,
       gender: 1,
@@ -41,7 +41,7 @@ export class AppComponent {
       status: 0
     },
     {
-      id: '15',
+      id: 15,
       name: "minh",
       age: 35,
       gender: 0,
@@ -69,5 +69,46 @@ export class AppComponent {
    changeInput(event: any) {
     this.inputValue =  event.target.value;
       
+   }
+// định nghĩa hàm trong form
+  inputValues = {
+    
+    name: '',
+    age: '',
+    gender : '0',
+    avatar: '',
+    status: '0'
+
+  }
+  //  onInputName(event: any, info: string) {
+  //   this.inputValues.name = event.target.value;
+     
+  //  }
+  //  onInputAge(event: any, info: string) {
+  //   this.inputValues.age = event.target.value;
+
+  //  }
+
+  // key: 'name'|'age' => key chỉ được là giá trị của name hoặc age
+   onInput(event:any, key: 'name'|'age'|'avatar'|'gender'|'status') {
+     this.inputValues[key] = event.target.value;
+   }
+   // sự kiện click vào nút submit
+   onSubmit(){
+    console.log(this.inputValues);
+    //thêm dữ liệu vừa thao tác vs form vào mảng teachers
+    this.teachers.push({...this.inputValues, 
+      age:+this.inputValues.age,// đưa age từ chuỗi về số
+      gender: +this.inputValues.gender,
+      status : +this.inputValues.status,
+    id: this.teachers.length +1});
+    // cập nhật lại gt cho input ở form
+    this.inputValues = {
+      name: '',
+      age: '',
+      gender: '0',
+      avatar:'',
+      status: '0'
+    }
    }
 }
